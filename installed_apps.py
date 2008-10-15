469df30b7ce02596ff094904cc20f3cde61d1cf8
Larry Jones <mrwizard82d1@earthlink.net>
2008-10-15 22:27:57 +0000

    - Repaired defects in InstalledApps.find(). - Changed command line driver to use InstalledApps.find().

--- installed_apps.py
+++ installed_apps.py
@@ -20,7 +20,7 @@ UNINSTALL_KEY='UninstallString'
 class InstalledApps(object):
     """Models the installed applications."""
     
-    def find(filter='.*', what_info=[VERSION_KEY],
+    def find(self, filter='.*', what_info=[VERSION_KEY],
              include_updates=False):
         """o.find(filter, what_info, include_updates) -> seq
 
@@ -31,15 +31,15 @@ class InstalledApps(object):
         Note that the list always includes 'DisplayName' if available. 
         include_updates - Flag if result includes Windows updates
         (default=False)."""
-        
+
         # Create a registry dictionary to query.
         keypath = os.path.join('Software', 'Microsoft', 'Windows',
                                'CurrentVersion', 'Uninstall')
         rd = reg_dict.RegistryDict(keypath=keypath,
                                    flags=win32con.KEY_ALL_ACCESS)
 
-        what_info.append[NAME_KEY]
-        raw_result = []
+        what_info.append(NAME_KEY)
+        all_installed = []
         for k in rd.keys():
             item = rd[k]
 
@@ -48,16 +48,17 @@ class InstalledApps(object):
                 if key in rd[k]:
                     info[key] = item[key]
 
-            raw_result.append(info)
+            all_installed.append(info)
 
-        updates_result = raw_result
-        if not include_updates :
-            regexp = re.compile('KB\d+')
-            update_result = [info for info in raw_result if
-                             not regexp.search(info[NAME_KEY])]
+        updates_pattern = re.compile('KB\d+')
+        updates_installed = all_installed if \
+                            include_updates else \
+                            [info for info in all_installed if
+                             NAME_KEY in info and
+                             not updates_pattern.search(info[NAME_KEY])]
 
-        regexp = re.compile(filter, re.IGNORECASE)
-        result = [info for info in updates_result if
-                  regexp.search(info[NAME_KEY])]
+        filter_pattern = re.compile(filter, re.IGNORECASE)
+        result = [info for info in updates_installed if
+                  filter_pattern.search(info[NAME_KEY])]
         return result
     
--- manage_installed_apps.py
+++ manage_installed_apps.py
@@ -10,38 +10,7 @@ import re
 
 import win32con
 
-import reg_dict
-
-
-def query_uninstallable(filter='.*', include_updates=False):
-    # Create a registry dictionary to query.
-    keypath = 'Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\'
-    rd = reg_dict.RegistryDict(keypath=keypath,
-                               flags=win32con.KEY_ALL_ACCESS)
-    raw_result = []
-    for k in rd.keys():
-        item = rd[k]
-        
-        display_name = ''
-        if 'DisplayName' in rd[k]:
-            display_name = item['DisplayName']
-            
-        display_version = ''
-        if 'DisplayVersion' in rd[k]:
-            display_version = item['DisplayVersion']
-            
-        raw_result.append((display_name, display_version))
-    
-    updates_result = raw_result
-    if not include_updates:
-        regexp = re.compile('KB\d+')
-        updates_result = [(name, version) for name, version in
-                          raw_result if not regexp.search(name)]
-      
-    regexp = re.compile(filter, re.IGNORECASE)
-    result = [(name, version) for name, version in updates_result if
-              regexp.search(name)]
-    return result
+from installed_apps import InstalledApps
 
 
 if __name__ == '__main__':
@@ -51,6 +20,7 @@ if __name__ == '__main__':
                       help='Select applications of interest (default=all).')
     (options, args) = parser.parse_args()
     
-    installed = query_uninstallable(filter=options.text)
+    ia = InstalledApps()
+    installed = ia.find(filter=options.text)
     installed.sort()
     pprint.pprint(installed)
