b9d992151d9ff67e4f683caed02413c91f82b98a
Larry Jones <mrwizard82d1@earthlink.net>
2009-04-25 02:46:01 +0000

    - Updated function to run under Python 3.0.

--- dirname_to_svn_url.py
+++ dirname_to_svn_url.py
@@ -1,4 +1,7 @@
-#! python
+#! env python
+
+
+"""Script to convert a directory name to an SVN URL."""
 
 
 from optparse import OptionParser
@@ -8,14 +11,20 @@ import os
 def dirname_to_svn_url(dirname):
     abs_dirname = os.path.abspath(dirname)
     drive, pathname = os.path.splitdrive(abs_dirname)
-    result = "file:///{0}{1}".format(drive, pathname.replace("\\", "/"))
+    result = "file:///%s%s/" % (drive, pathname.replace("\\", "/"))
     return result
 
 
 if __name__ == '__main__':
-    usage = '%prog [options] dirname'
-    parser = OptionParser()
-    opts, args = parser.parse_args()
+    usage = """%prog [options] dirname
+
+    Converts a directory name to an SVN URL.
+    """
+    parser = OptionParser(usage=usage)
+    (opts, args) = parser.parse_args();
+    if (len(args) != 1):
+        parser.error("No dirname supplied.");
+        
     svn_url = dirname_to_svn_url(args[0])
     print(svn_url)
     
