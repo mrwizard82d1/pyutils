786e52a107fc2efe8f7f8ab90af113ef31039738
Larry Jones <l.jones@sigmacubed.com>
2014-04-07 22:28:40 +0100

    Changed path2list to handle piping in a path.

--- path2list.py
+++ path2list.py
@@ -20,12 +20,14 @@ if __name__ == '__main__':
     usage = "Usage: %prog [options]"
     parser = OptionParser(usage)
     parser.add_option('-p', '--path', dest='path', default='PATH',
-                      help="Path to list (default = 'PATH')")
+                      help="Path to list (default = 'PATH' '-' for stdin)")
     (options, args) = parser.parse_args()
     if (len(args) > 0):
         parser.error('No arguments expected: {0}'.format(args))
 
-    thePath = os.environ[ options.path.upper() ]
+    thePath = (os.environ[ options.path.upper() ] if
+               options.path != '-' else
+               sys.stdin.read())
     theListing = path2list( thePath )
     print(theListing)
     
